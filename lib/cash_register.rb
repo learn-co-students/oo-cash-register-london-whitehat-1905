ca877f0a85ec57d879f3ac73e7e11a93e5f25c32
NJ-JayJay <metalmedley1@gmail.com>
2019-05-07 21:28:50 +0100

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+    attr_accessor   :total, :discount
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+        for i in (0...quantity)
+            @items << title
+        end
+    end
+
+    def apply_discount
+        if @discount == 0
+            return "There is no discount to apply."
+        else
+        @total *= (1 - (@discount/100.0))
+        #integer division always rounds down
+        #using 100 as a float guarentees the result will be a float
+        "After the discount, the total comes to $#{@total.to_i}."
+        end
+    end 
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total = 0
+    end
+
+end
 
